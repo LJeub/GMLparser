4135adabde3452c6ea3654669c5e65e723d72c52
Lucas Jeub <jeub@maths.ox.ac.uk>
2013-12-02 19:39:19 +0000

    cleaned up but still slow

--- gml2struct.m
+++ gml2struct.m
@@ -1,108 +1,122 @@
-function [datastruct]=gml2struct(filename)
+function [gmlstruct]=gml2struct(filename)
 
 file=fopen(filename);
 
-
 % initialise variables
 level=1;
-datastruct=[];
+gmlstruct=[];
 multiplicity={};
-lastfield={};
+currentpos={};
 
-tline=fgetl(file);
+tline=(fgetl(file));
 while ischar(tline)
     tline=strtrim(tline);
     if ~isempty(tline)  %ignore empty lines
-        if strncmp(tline,'#',1) %ignore comment lines
-            tline=[];
-        else
-            [field,~,~,index]=sscanf(tline,'%s',1);
-            
-            % checks for increment/decrement of level ([,])
-            if strcmp(field,'[')
+        switch tline(1)
+            case '#'
+                tline=[]; %ignore comment lines
+                
+            case '['
+                % checks for increment of level
                 level=level+1;
                 tline=strtrim(tline(2:end));
-            elseif strcmp(field,']')
+            case ']'
+                % checks for decrement of level
                 level=level-1;
                 tline=strtrim(tline(2:end));
-            else
-                
-                % keep track of multiple fields with same name (such as all the nodes)
-                % lastfield tracks the position in the tree
-                if length(multiplicity)<level||length(lastfield)<2*level % field is new
-                    multiplicity{level}.(field)=1;
-                elseif isfield(getfield(datastruct,lastfield{1:2*level-2}),field) % check if field exists
-                    multiplicity{level}.(field)=multiplicity{level}.(field)+1;
-                else % field is new
-                    multiplicity{level}.(field)=1;
-                end
-                lastfield{2*level-1}=field;
-                
-                
-                % deal with quirks in matlab structure
-                
-                % if field is new
-                if multiplicity{level}.(field)==1
-                    lastfield=lastfield(1:2*level-1);
-                else
-                    % if field already exists
-                    lastfield{2*level}={multiplicity{level}.(field)};
-                    lastfield=lastfield(1:2*level);
-                end
-                
-                % insert index for fields created at the last step
-                if level>1
-                    if isempty(lastfield{2*level-2})
-                        lastfield{2*level-2}={1};
-                    end
-                end
-                
-                tline=tline(index:end);
-                
-                % get value of key (if two values specified only second is
-                % kept)
-                while ~isempty(tline)
-                    
-                    % extract field if it is numeric
-                    [field,~,~,index]=sscanf(tline, '%f',1);
-                    tline=tline(index:end);    % if not numeric index gives first non-whitespace character
-                    if ~isempty(field)
-                        datastruct=setfield(datastruct,lastfield{:},field);
-                        
-                        % extract field if it is a string delmited by ""
-                    elseif strcmp(tline(1),'"')
-                        index=2;
-                        it=1;
-                        while (~strcmp(tline(index),'"'))&&(index<=length(tline))
-                            field(it)=tline(index);
-                            index=index+1;
-                            it=it+1;
-                        end
-                        index=index+1;
-                        datastruct=setfield(datastruct,lastfield{:},char(field));
-                        
-                        % checks for increment/decrement of level ([,])
-                    else
-                        [field,~,~,index]=sscanf(tline, '%s',1);
-                        if strcmp(field,'[')
-                            level=level+1;
-                            tline=strtrim(tline(2:end));
-                            break
-                        elseif strcmp(field,']')
-                            level=level-1;
-                            tline=strtrim(tline(2:end));
-                            break
-                        end
-                    end
-                    tline=tline(index:end);
-                end
+        end
+        if level==0
+            error('tree level reached 0, root is at level 1. Check file for missing [,],"')
+        end
+    end
+    
+    if ~isempty(tline)
+        % get key
+        it=0;
+        while (it<length(tline))&&~(tline(it+1)==' ')
+            it=it+1;
+        end
+        key=tline(1:it);
+        if ~isvarname(key)
+            varkey=genvarname(key);
+            warning('%s is not a valid field name for a struct, changed it to %s\n',key,varkey);
+            key=varkey;
+        end
+        tline=strtrim(tline(it+1:end));
+        
+        % keep track of multiple fields with same name (such as all the nodes)
+        % currentpos tracks the position in the tree
+        if length(multiplicity)<level||length(currentpos)<2*level % key is new at this level
+            multiplicity{level}.(key)=1;
+        elseif isfield(multiplicity{level},key) % check if key exists
+            multiplicity{level}.(key)=multiplicity{level}.(key)+1; % track number of times key has appeared
+        else % key is new
+            multiplicity{level}.(key)=1;
+        end
+        currentpos{2*level-1}=key;
+        
+        
+        % deal with quirks in matlab structure
+        
+        % if field is new
+        if multiplicity{level}.(key)==1
+            currentpos=currentpos(1:2*level-1);
+        else
+            % if field already exists
+            currentpos{2*level}={multiplicity{level}.(key)};
+            currentpos=currentpos(1:2*level);
+        end
+        
+        % insert index for fields created at the last step
+        if level>1
+            if isempty(currentpos{2*level-2})
+                currentpos{2*level-2}={1};
             end
         end
     end
     
+    % get value of key
+    if ~isempty(tline)
+        % value is a string
+        if tline(1)=='"'
+            tline=tline(2:end);
+            if isempty(tline)
+                tline=fgetl(file);
+                if ~ischar(tline)
+                    error('file ended while reading string');
+                else
+                    tline=strtrim(tline);
+                end
+            end
+            it=1;
+            value=[];
+            while tline(it)~='"'
+                if length(tline)==it
+                    value=[value,{tline}];
+                     tline=fgetl(file);
+                    if ~ischar(tline)
+                        error('file ended while reading string');
+                    else
+                        tline=strtrim(tline);
+                    end
+                    it=1;
+                else
+                    it=it+1;
+                end
+            end
+            value=[value,{tline(1:it-1)}];
+            tline=strtrim(tline(it+1:end));
+        else
+            % extract value if it is numeric
+            [value,~,~,index]=sscanf(tline, '%f',1);
+            tline=tline(index:end);    % index gives first non-whitespace character after extracted value
+        end
+        gmlstruct=setfield(gmlstruct,currentpos{:},value);
+    end
+            
     if isempty(tline)
         tline=fgetl(file);
     end
 end
-
 end
+
