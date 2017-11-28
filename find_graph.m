e79289903a0035b0d9c9006c50f86bb363ac9be7
Lucas Jeub <ljeub@iu.edu>
2017-11-28 17:30:33 -0500

    Fixed to work if gmlstruct is already a graph and to handle weird input

--- find_graph.m
+++ find_graph.m
@@ -1,11 +1,24 @@
 function graph=find_graph(gmlstruct)
+% graph=find_graph(gmlstruct) finds graph in gmlstruct
+%
+% Recursively iterates through gmlstruct until it finds the graph and
+% returns it. If no graph is found, returns empty array.
 graph=[];
-if ~isfield(gmlstruct,'graph')
-    fields=fieldnames(gmlstruct);
-    for i=1:length(fields)
-        graph=find_graph(gmlstruct.(fields{i}));
+if isstruct(gmlstruct)
+    if isfield(gmlstruct,'node')
+        % gmlstruct is already a graph
+        graph=gmlstruct;
+    elseif~isfield(gmlstruct,'graph')
+        % no graph at top level
+        fields=fieldnames(gmlstruct);
+        i=1;
+        while isempty(graph)&&i<=length(fields)
+            graph=find_graph(gmlstruct.(fields{i}));
+            i=i+1;
+        end
+    else
+        % graph at top level
+        graph=gmlstruct.graph;
     end
-else
-    graph=gmlstruct.graph;
 end
 end
