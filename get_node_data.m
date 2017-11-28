2baa5fab6867514108efa7286bc27a06cd7105dc
Lucas Jeub <ljeub@iu.edu>
2017-11-28 17:48:18 -0500

    add documentation

--- get_node_data.m
+++ get_node_data.m
@@ -1,4 +1,9 @@
 function data=get_node_data(gml,field)
+% data=get_node_data(gml,field) extracts node data from gml struct
+%
+% Convenience function for extracting node data. It finds the graph first
+% (does not have to be in the top level of 'gml') and returns data as a
+% cell array or numeric array depending on the data type.
 
 graph=find_graph(gml);
 if ischar(graph.node(1).(field))
